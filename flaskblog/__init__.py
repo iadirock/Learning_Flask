b22e2a83d0b73df6f3ec7db396b5271dbda836bf
iadirock <aditya16219@iiitd.ac.in>
2021-02-03 00:26:18 +0530

    Add registration of the user in the Database

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c4ad0f02dad6d26e22ee3ed0a70777cf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
